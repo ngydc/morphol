0026d224f4b02a1561e7b3bdd51a0e7662240002
Duc Anh Nguyen <ducanh08@gmx.de>
2021-05-01 15:34:40 +0200

    added variants with random window sizes

--- main.py
+++ main.py
@@ -8,21 +8,42 @@ def main():
     image = Image.open(img_path).convert('L')
     img_mat = np.array(image, dtype=np.float64)
 
-    eroded = morphol.erode(img_mat, 40)
+    """ standard morphological filters """
+
+    eroded = morphol.erode(img_mat, 20)
     eroded_img = Image.fromarray(np.uint8(eroded), 'L')
     eroded_img.save(img_path.replace('.jpg', '_eroded.jpg'), format='JPEG')
 
-    dilated = morphol.dilate(img_mat, 40)
+    dilated = morphol.dilate(img_mat, 20)
     dilated_img = Image.fromarray(np.uint8(dilated), 'L')
     dilated_img.save(img_path.replace('.jpg', '_dilated.jpg'), format='JPEG')
 
-    closed = morphol.close(img_mat, 40)
+    closed = morphol.close(img_mat, 20)
     closed_img = Image.fromarray(np.uint8(closed), 'L')
     closed_img.save(img_path.replace('.jpg', '_closed.jpg'), format='JPEG')
 
-    opened = morphol.open(img_mat, 40)
+    opened = morphol.open(img_mat, 20)
     opened_img = Image.fromarray(np.uint8(opened), 'L')
     opened_img.save(img_path.replace('.jpg', '_opened.jpg'), format='JPEG')
+
+    """ morphological filters with random window sizes"""
+    """
+    eroded_rand = morphol.erode_random(img_mat, 10, 20)
+    eroded_img_rand = Image.fromarray(np.uint8(eroded_rand), 'L')
+    eroded_img_rand.save(img_path.replace('.jpg', '_eroded_rand.jpg'), format='JPEG')
+
+    dilated_rand = morphol.dilate_random(img_mat, 10, 20)
+    dilated_img_rand = Image.fromarray(np.uint8(dilated_rand), 'L')
+    dilated_img_rand.save(img_path.replace('.jpg', '_dilated_rand.jpg'), format='JPEG')
+
+    opened_rand = morphol.open_random(img_mat, 10, 20)
+    opened_img_rand = Image.fromarray(np.uint8(opened_rand), 'L')
+    opened_img_rand.save(img_path.replace('.jpg', '_opened_rand.jpg'), format='JPEG')
+
+    closed_rand = morphol.close_random(img_mat, 10, 20)
+    closed_img_rand = Image.fromarray(np.uint8(closed_rand), 'L')
+    closed_img_rand.save(img_path.replace('.jpg', '_closed_rand.jpg'), format='JPEG')
+    """
 
 
 if __name__ == '__main__':
--- morphol.py
+++ morphol.py
@@ -1,36 +1,78 @@
-import math
 import numpy as np
+import numpy.random
 
 
-def erode(image, window):
-    return _filter(image, window, 'erosion')
+def erode(image, radius):
+    return _filter(image, radius, 'erosion')
 
 
-def dilate(image, window):
-    return _filter(image, window, 'dilation')
+def dilate(image, radius):
+    return _filter(image, radius, 'dilation')
 
 
-def close(image, window):
-    dilated = dilate(image, window)
-    return erode(dilated, window)
+def close(image, radius):
+    dilated = dilate(image, radius)
+    return erode(dilated, radius)
 
 
-def open(image, window):
-    eroded = erode(image, window)
-    return dilate(eroded, window)
+def open(image, radius):
+    eroded = erode(image, radius)
+    return dilate(eroded, radius)
 
 
-def _filter(image, window, morph_type):
+def erode_random(image, r_min, r_max):
+    return _filter_random(image, r_min, r_max, 'erosion')
+
+
+def dilate_random(image, r_min, r_max):
+    return _filter_random(image, r_min, r_max, 'dilation')
+
+
+def close_random(image, r_min, r_max):
+    dilated = dilate_random(image, r_min, r_max)
+    return erode_random(dilated, r_min, r_max)
+
+
+def open_random(image, r_min, r_max):
+    eroded = erode_random(image, r_min, r_max)
+    return dilate_random(eroded, r_min, r_max)
+
+
+def _filter(image, radius, morph_type):
     m, n = image.shape
-    pad = int(math.floor(window / 2))
+    pad = radius
     padded = np.pad(image, ((pad, pad), (pad, pad)), 'edge')
     tmp = np.zeros((m, n))
     if morph_type == 'erosion':
         for i, j in np.ndindex(tmp.shape):
-            tmp[i, j] = np.min(padded[i:i + window, j:j + window])
+            tmp[i, j] = np.min(padded[i:i + radius*2-1, j:j + radius*2-1])
     elif morph_type == 'dilation':
         for i, j in np.ndindex(tmp.shape):
-            tmp[i, j] = np.max(padded[i:i + window, j:j + window])
+            tmp[i, j] = np.max(padded[i:i + radius*2-1, j:j + radius*2-1])
     else:
         raise Exception('Incorrect morphological type given.')
     return tmp
+
+
+def _filter_random(image, r_min, r_max, morph_type):
+    m, n = image.shape
+    padded = np.pad(image, ((r_max, r_max), (r_max, r_max)), 'edge')
+    tmp = np.zeros((m, n))
+    if morph_type == 'erosion':
+        for i, j in np.ndindex(tmp.shape):
+            i_idx, j_idx = _get_rand_idx(i, j, r_min, r_max)
+            tmp[i, j] = np.min(padded[i_idx, j_idx])
+    elif morph_type == 'dilation':
+        for i, j in np.ndindex(tmp.shape):
+            i_idx, j_idx = _get_rand_idx(i, j, r_min, r_max)
+            tmp[i, j] = np.max(padded[i_idx, j_idx])
+    else:
+        raise Exception('Incorrect morphological type given.')
+    return tmp
+
+
+def _get_rand_idx(i, j, r_min, r_max):
+    r_rand = numpy.random.randint(r_min, r_max)
+    i_idx = slice(i - r_rand + r_max - 1, i + r_rand + r_max - 1)
+    j_idx = slice(j - r_rand + r_max - 1, j + r_rand + r_max - 1)
+    return i_idx, j_idx
